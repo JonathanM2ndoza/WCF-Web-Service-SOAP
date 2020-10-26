168636500029b38c99320fbb7d76aaab1c1607ac
jmendoza <jonathan.m2ndoza@gmail.com>
2020-10-26 16:25:12 -0400

    Create basic WCF project

---
+++ Demo-WS-SOAP/Demo-WS-SOAP/Services/ITestService.cs
@@ -0,0 +1,20 @@
+﻿using Demo_WS_SOAP.Domain;
+using System.ServiceModel;
+
+namespace Demo_WS_SOAP.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITestService" in both code and config file together.
+    [ServiceContract]
+    public interface ITestService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+}
---
+++ Demo-WS-SOAP/Demo-WS-SOAP/Services/TestService.svc.cs
@@ -0,0 +1,28 @@
+﻿using Demo_WS_SOAP.Domain;
+using System;
+
+namespace Demo_WS_SOAP.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TestService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select TestService.svc or TestService.svc.cs at the Solution Explorer and start debugging.
+    public class TestService : ITestService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
