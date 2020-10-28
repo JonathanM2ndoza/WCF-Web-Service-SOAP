545643f9f21225226750c2a8996d60656f3503cc
jmendoza <jonathan.m2ndoza@gmail.com>
2020-10-28 09:21:51 -0400

    WCF Security - Custom Credentials

---
+++ Security-WS-SOAP/Security-SOAP-Client/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Security_SOAP_Client
+{
+    public partial class Form1 : Form
+    {
+
+        ReferenceTestService.TestServiceClient testServiceClient = new ReferenceTestService.TestServiceClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string resultGetData = testServiceClient.GetData(Int32.Parse(textBox1.Text));
+            MessageBox.Show(resultGetData);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Trust all certificates
+            System.Net.ServicePointManager.ServerCertificateValidationCallback =
+            (send, certificate, chain, sslPolicyErrors) => true;
+
+            testServiceClient.ClientCredentials.UserName.UserName = "jmendoza";
+            testServiceClient.ClientCredentials.UserName.Password = "123456";
+        }
+    }
+}
---
+++ Security-WS-SOAP/Security-WS-SOAP/Security/CustomUserNameValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IdentityModel.Selectors;
+using System.ServiceModel;
+
+namespace Security_WS_SOAP.Security
+{
+    public class CustomUserNameValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentNullException("User and Password can not be empty.");
+            }
+
+            // Go to the Repository
+            if (!(userName.Equals("jmendoza", StringComparison.InvariantCultureIgnoreCase)
+                && password.Equals("123456", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                // This throws an informative fault to the client.
+                throw new FaultException("Unknown Username or Incorrect Password");
+                // When you do not want to throw an infomative fault to the client,
+                // throw the following exception.
+                // throw new SecurityTokenException("Unknown Username or Incorrect Password");
+            }
+        }
+    }
+}
